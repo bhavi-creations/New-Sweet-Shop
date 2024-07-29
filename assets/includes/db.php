10ea45f11c2b517754d233f3bb0f4236c1bd937e
sarikapatnala <sarikapatnala2@gmail.com>
2024-07-29 10:55:27 +0530

    billing insert query

---
+++ assets/includes/db.php
@@ -0,0 +1,9 @@
+<?php
+$db_con = mysqli_connect('localhost', 'root', '', 'sweet_shop');
+
+// Check the connection
+// if ($db_con) {
+//     echo '<script>alert("DB Connected Successfully")</script>';
+// } else {
+//     echo '<script>alert("Failed To Connect: ' . mysqli_connect_error() . '")</script>';
+// }
--- billings.php
+++ billings.php
@@ -4,6 +4,48 @@
     <?php
     include "assets/includes/sidebar.php";
     include "assets/includes/header.php";
+    include "assets/includes/db.php";
+
+    if (isset($_POST['submit-btn'])) {
+
+        $iname = mysqli_real_escape_string($db_con, $_POST['Item']);
+        $kggs = mysqli_real_escape_string($db_con, $_POST['Kg']);
+        $amnt = mysqli_real_escape_string($db_con, $_POST['amount']);
+        $disc = mysqli_real_escape_string($db_con, $_POST['discount']);
+        $incName = mysqli_real_escape_string($db_con, $_POST['incharge']);
+        $brnch = mysqli_real_escape_string($db_con, $_POST['branch']);
+        $dat = mysqli_real_escape_string($db_con, $_POST['date']);
+
+        $billingQuery = mysqli_query($db_con, "INSERT INTO items SET ItemName='" . $iname . "',Kgs='" . $kggs . "',Amount='" . $amnt . "',Discount='" . $disc . "',InchargeName	='" . $incName . "',Branch='" . $brnch . "',Date='" . $dat . "', status=1");
+        if ($billingQuery) {
+            echo '<script type="text/javascript">
+        $(document).ready(function() {
+            swal({
+                title: "Success...!",
+                text: "Data Inserted Successfully!!",
+                icon: "success",
+                button: "Ok",
+            });
+        });
+    </script>';
+        } else {
+            echo '<script type="text/javascript">
+        $(document).ready(function() {
+            swal({
+                title: "Failed...!",
+                text: "Failed to Insert!!",
+                icon: "error",
+                button: "Ok",
+            });
+        });
+    </script>';
+        }
+    }
+
+
+
+
+
     ?>
 
 
@@ -20,7 +62,7 @@
                             <ul class="ul_style">
                                 <li id="addReport" class="add_staff_list_detils open_table">+ Add Item</li>
 
-                                <li id="reports" class="staff_list_detils open_table active">Items</li>
+                                <!-- <li id="reports" class="staff_list_detils open_table active">Items</li> -->
                                 <li id="totalBranch" class="staff_list_detils open_table">Total Items</li>
 
                             </ul>
@@ -82,37 +124,38 @@
 
                                             <div class="col-md-6 mt-5">
                                                 <label class="control-label mb-2 field_txt">Item Name</label>
-                                                <input type="text" class="form-control field_input_bg" name="address">
+                                                <input type="text" class="form-control field_input_bg" name="Item">
 
                                             </div>
                                             <div class="col-md-6  mt-5">
                                                 <label class="control-label mb-2 field_txt">Kg's</label>
-                                                <input type="text" class="form-control field_input_bg" name="age">
+                                                <input type="text" class="form-control field_input_bg" name="Kg">
                                             </div>
                                             <div class="col-md-6  mt-5">
                                                 <label class="control-label mb-2 field_txt">Amount</label>
-                                                <input type="number" class="form-control field_input_bg" name="account">
+                                                <input type="number" class="form-control field_input_bg" name="amount">
 
 
                                             </div>
                                             <div class="col-md-6  mt-5">
                                                 <label class="control-label mb-2 field_txt">Discount</label>
-                                                <input type="number" class="form-control field_input_bg" name="phone">
+                                                <input type="number" class="form-control field_input_bg"
+                                                    name="discount">
 
 
                                             </div>
                                             <div class="col-md-6 mt-5">
                                                 <label class="control-label mb-2 field_txt">Incharge Name</label>
-                                                <input type="text" class="form-control field_input_bg" name="address">
+                                                <input type="text" class="form-control field_input_bg" name="incharge">
 
                                             </div>
                                             <div class="col-md-6  mt-5">
                                                 <label class="control-label mb-2 field_txt">Branch</label>
-                                                <input type="text" class="form-control field_input_bg" name="age">
+                                                <input type="text" class="form-control field_input_bg" name="branch">
                                             </div>
                                             <div class="col-md-6  mt-5">
                                                 <label class="control-label mb-2 field_txt">Date</label>
-                                                <input type="Date" class="form-control field_input_bg" name="age">
+                                                <input type="Date" class="form-control field_input_bg" name="date">
                                             </div>
 
                                             <div class="col-md-6 mt-5">
@@ -121,7 +164,7 @@
                                                 <div
                                                     class="row last_back_submit  d-flex flex-row justify-content-between  px-3">
                                                     <button class="back_btn_staff">Back</button>
-                                                    <button class="submit_btn_staff">Submit</button>
+                                                    <button class="submit_btn_staff" name="submit-btn">Submit</button>
 
                                                 </div>
 
@@ -134,7 +177,7 @@
                                 </form>
                             </div>
 
-                            <div id="reportsTable" class="table-container active">
+                            <!-- <div id="reportsTable" class="table-container active">
 
 
                                 <div class="container">
@@ -144,11 +187,8 @@
                                         </div>
                                         <div class="">
                                             <h6 class="kkd_brnch">Kakinada Branch
-                                                <svg class="kkdIcon ml-3" xmlns="http://www.w3.org/2000/svg" width="20"
-                                                    height="19" viewBox="0 0 20 19" fill="none">
-                                                    <path
-                                                        d="M17.7178 4.96555L10 12.1861L2.28216 4.96555C1.67358 4.33767 1.06501 4.32459 0.456432 4.9263C-0.152144 5.52802 -0.152144 6.11665 0.456432 6.6922L9.08714 14.8546C9.30844 15.1162 9.61272 15.247 10 15.247C10.3873 15.247 10.6916 15.1162 10.9129 14.8546L19.5436 6.6922C20.1521 6.11665 20.1521 5.52802 19.5436 4.9263C18.935 4.32459 18.3264 4.33767 17.7178 4.96555Z"
-                                                        fill="#202224" />
+                                                <svg class="kkdIcon ml-3" xmlns="http://www.w3.org/2000/svg" width="20" height="19" viewBox="0 0 20 19" fill="none">
+                                                    <path d="M17.7178 4.96555L10 12.1861L2.28216 4.96555C1.67358 4.33767 1.06501 4.32459 0.456432 4.9263C-0.152144 5.52802 -0.152144 6.11665 0.456432 6.6922L9.08714 14.8546C9.30844 15.1162 9.61272 15.247 10 15.247C10.3873 15.247 10.6916 15.1162 10.9129 14.8546L19.5436 6.6922C20.1521 6.11665 20.1521 5.52802 19.5436 4.9263C18.935 4.32459 18.3264 4.33767 17.7178 4.96555Z" fill="#202224" />
                                                 </svg>
                                             </h6>
                                         </div>
@@ -181,16 +221,13 @@
                                             <td class="td_id_mob">3000</td>
                                             <td class="td_id_num">14/08/2024</td>
                                             <td>
-                                                <button class="edit_icon"><i
-                                                        class="fa-regular fa-pen-to-square"></i></button>
-                                                <button class="dlt_icon"><i
-                                                        class="fa-regular fa-trash-can"></i></button>
+                                                <button class="edit_icon"><i class="fa-regular fa-pen-to-square"></i></button>
+                                                <button class="dlt_icon"><i class="fa-regular fa-trash-can"></i></button>
                                             </td>
                                         </tr>
-                                        <!-- Add more rows as needed -->
                                     </tbody>
                                 </table>
-                            </div>
+                            </div> -->
 
                             <div id="totalBranchTable" class="table-container  ">
 
@@ -217,21 +254,33 @@
                                     <thead class="table_bg">
                                         <tr>
                                             <th class="th_names">ID</th>
-                                            <th class="th_names">Items</th>
+                                            <th class="th_names">Item Name</th>
                                             <th class="th_names">Kg's</th>
+                                            <th class="th_names">Amount</th>
+                                            <th class="th_names">Discount</th>
+                                            <th class="th_names">Incharge Name</th>
+                                            <th class="th_names">Branch</th>
+                                            <th class="th_names">Date</th>
                                             <th class="th_names">Actions</th>
 
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr class="tr_hover">
-                                            <td class="td_id_num">1</td>
+                                        <?php
+                                        $getQuery = mysqli_query($db_con, "SELECT * FROM items WHERE status = 1");
+                                        $no = 1;
+                                        while ($data = mysqli_fetch_array($getQuery)) {
 
-                                            <td class="td_id_num">
-                                                <img src="img/profile (1).png" alt="John Doe" class="td_profile_pic">
-                                                Kaja
-                                            </td>
-                                            <td class="td_id_mob">55</td>
+                                        ?>
+                                        <tr class="tr_hover">
+                                            <td class="td_id_num"><?php echo $no ?></td>
+                                            <td class="td_id_mob"><?php echo $data['ItemName'] ?></td>
+                                            <td class="td_id_mob"><?php echo $data['Kgs'] ?></td>
+                                            <td class="td_id_mob"><?php echo $data['Amount'] ?></td>
+                                            <td class="td_id_mob"><?php echo $data['Discount'] ?></td>
+                                            <td class="td_id_mob"><?php echo $data['InchargeName'] ?></td>
+                                            <td class="td_id_mob"><?php echo $data['Branch'] ?></td>
+                                            <td class="td_id_mob"><?php echo $data['Date'] ?></td>
 
                                             <td>
                                                 <button class="edit_icon"><i
@@ -240,6 +289,10 @@
                                                         class="fa-regular fa-trash-can"></i></button>
                                             </td>
                                         </tr>
+                                        <?php
+                                            $no++;
+                                        }
+                                        ?>
                                         <!-- Add more rows as needed -->
                                     </tbody>
                                 </table>
